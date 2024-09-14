23e9e1d7097329cd3679bc6b11b46e38895c5771
jg10 <181532694+jg10-mastodon-social@users.noreply.github.com>
2024-09-14 01:24:27 +0000

    Support a uri template; app class with uri method; remove duplicate entries

--- OpenWith.js
+++ OpenWith.js
@@ -1,3 +1,78 @@
+class App {
+  constructor(kb, appNode) {
+    this.kb = kb;
+    this.appNode = appNode;
+    this.ns = {
+      solid: kb.ns("http://www.w3.org/ns/solid/terms#"),
+      apps: kb.ns("https://jg10.solidcommunity.net/open-with/apps.ttl#"),
+      rdfs: kb.ns("http://www.w3.org/2000/01/rdf-schema#"),
+    };
+  }
+
+  uri(resourceUri, rego) {
+    let uri;
+    uri = this.uriFromPrefix(resourceUri);
+    if (uri != null) return uri;
+
+    uri = this.uriFromTemplate(resourceUri, rego);
+    if (uri != null) return uri;
+
+    return null;
+  }
+
+  uriFromPrefix(resourceUri) {
+    let uriPrefix = this.kb.anyValue(
+      this.appNode,
+      this.ns.apps("uriPrefix"),
+      null,
+    );
+    if (uriPrefix) {
+      return uriPrefix + encodeURIComponent(resourceUri);
+    }
+    return null;
+  }
+
+  /**
+   * Uses a regexp replacement to create an app uri from a resourceUri
+   *
+   * :app apps:resourceUriTemplate defines a RegExp to parse the provided resourceUri. Additional substitutions are provided:
+   *    $instanceContainer is substituted by rego solid:instanceContainer (note that app may be used in several registrations)
+   * :app apps:uriTemplate is used to create an app uri and can use regexp capture groups from resourceUriTemplate
+   *
+   * e.g.
+   *
+   * :Umai
+   *     rdfs:label "Umai";
+   *     apps:resourceUriTemplate ".*$instanceContainer(.*)#it";
+   *     apps:uriTemplate "https://umai.noeldemartin.com/recipes/$1/".
+   */
+  uriFromTemplate(resourceUri, rego) {
+    let resourceUriTemplate = this.kb.anyValue(
+      this.appNode,
+      this.ns.apps("resourceUriTemplate"),
+      null,
+    );
+    if (resourceUriTemplate) {
+      let uriTemplate = this.kb.anyValue(
+        this.appNode,
+        this.ns.apps("uriTemplate"),
+        null,
+      );
+      if (rego) {
+        let instanceContainer = this.kb.anyValue(
+          rego,
+          this.ns.solid("instanceContainer"),
+        );
+        resourceUriTemplate = resourceUriTemplate.replace(
+          "$instanceContainer",
+          instanceContainer,
+        );
+      }
+      return resourceUri.replace(new RegExp(resourceUriTemplate), uriTemplate);
+    }
+  }
+}
+
 class OpenWith extends ReceiveResourceOS {
   update() {
     if (!this.resource || !this.os) {
@@ -5,41 +80,33 @@ class OpenWith extends ReceiveResourceOS {
     }
     this.replaceChildren([]);
 
-    let thisTypes = this.resource.types().map(x => x.uri);
     let kb = this.os.store.graph;
+    let ns = {
+      solid: kb.ns("http://www.w3.org/ns/solid/terms#"),
+      apps: kb.ns("https://jg10.solidcommunity.net/open-with/apps.ttl#"),
+      rdfs: kb.ns("http://www.w3.org/2000/01/rdf-schema#"),
+    };
+    let thisTypes = this.resource.types().map((x) => x.uri);
+    // Iterate through all solid:TypeRegistration-s for this resource's classes
     let apps = thisTypes
-      .flatMap(t =>
-        kb.each(
-          null,
-          kb.sym("http://www.w3.org/ns/solid/terms#forClass"),
-          kb.sym(t)
-        )
+      .flatMap((t) => kb.each(null, ns.solid("forClass"), kb.sym(t)))
+      .filter((rego) => rego != null)
+      .flatMap((rego) =>
+        kb.each(rego, ns.apps("apps"), null).flatMap((appNode) => {
+          let app = new App(kb, appNode);
+          return {
+            uri: app.uri(this.resource.uri, rego),
+            label: kb.anyValue(appNode, ns.rdfs("label"), null),
+          };
+        }),
       )
-      .filter(rego => rego != null)
-      .flatMap(rego =>
-        kb
-          .each(
-            rego,
-            kb.sym("https://jg10.solidcommunity.net/open-with/apps.ttl#apps"),
-            null
-          )
-          .flatMap(app => ({
-            uriPrefix: kb.anyValue(
-              app,
-              kb.sym(
-                "https://jg10.solidcommunity.net/open-with/apps.ttl#uriPrefix"
-              ),
-              null
-            ),
-            label: kb.anyValue(
-              app,
-              kb.sym("http://www.w3.org/2000/01/rdf-schema#label"),
-              null
-            )
-          }))
-      )
-      .filter(app => app != null);
-    console.log(apps);
+      .filter((app) => app != null && app.uri != null)
+      //Concise but quadratic time solution from https://stackoverflow.com/questions/9229645/remove-duplicate-values-from-js-array
+      //Assumes earlier registrations take precedence
+      .filter(function (item, pos, self) {
+        return self.indexOf(item) == pos;
+      });
+    //console.log(apps);
 
     if (apps.length > 0) {
       let select = document.createElement("ion-select");
@@ -47,14 +114,13 @@ class OpenWith extends ReceiveResourceOS {
       select.placeholder = "App";
       select.interface = "action-sheet";
       select.classList.add("ion-padding-horizontal");
-      select.addEventListener("ionChange", e => {
+      select.addEventListener("ionChange", (e) => {
         window.open(e.detail.value, "_blank");
       });
 
       for (let app of apps) {
-        let uri = app.uriPrefix + encodeURIComponent(this.resource.uri);
         let option = document.createElement("ion-select-option");
-        option.value = uri;
+        option.value = app.uri;
         option.innerText = app.label;
         select.append(option);
       }
